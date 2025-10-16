7fdbbddf17f6a3bf72c17bb82e246212a0eca163
axtodev <tony447113@gmail.com>
2025-10-16 23:10:14 +0200

    idk

---
+++ src/pages/auth/ResetPassword.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import ResetPasswordRequest from '../../components/ResetPasswordRequest';
+import '../../style/login.css';
+import '../../style/Root.css';
+
+function ResetPassword() {
+  return (
+    <div className="login">
+      <ResetPasswordRequest />
+    </div>
+  );
+}
+
+export default ResetPassword;
---
+++ src/pages/auth/ResetPasswordConfirm.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import ResetPasswordConfirmComponent from '../../components/ResetPasswordConfirm';
+import '../../style/login.css';
+import '../../style/Root.css';
+
+function ResetPasswordConfirm() {
+  return (
+    <div className="login">
+      <ResetPasswordConfirmComponent />
+    </div>
+  );
+}
+
+export default ResetPasswordConfirm;
