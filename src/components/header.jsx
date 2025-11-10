7b464df05dee6a81fb34288ca6f68e87e2030ff6
axtodev <tony447113@gmail.com>
2025-11-10 17:24:06 +0100

    fix

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,6 +1,3 @@
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
 import React from 'react';
 import '../style/header.css';
 import Logo from '../assets/logo.png';
@@ -41,49 +38,3 @@ function Head() {
 }
 
 export default Head;
-=======
->>>>>>> 0c76dc1 (Initial commit)
-import React from 'react';
-import '../style/header.css';
-import Logo from '../assets/logo.png';
-import { LogIn, UserPlus, LogOut, User } from 'lucide-react';
-import { useNavigate } from 'react-router-dom';
-
-function Head() {
-    const navigate = useNavigate();
-    const isLogged = localStorage.getItem('token');
-    const myUsername = localStorage.getItem('username'); 
-
-    const handleLogout = () => {
-        localStorage.removeItem('token');
-        localStorage.removeItem('username');
-        navigate('/login');
-    }
-
-    return (
-        <div className="header">
-            <div className="auth">
-                {isLogged ? (
-                    <>
-                        <button onClick={() => navigate(`/profile/${myUsername}`)}>
-                            <User /> Profilo
-                        </button>
-                        <button onClick={() => handleLogout()}><LogOut /> Logout</button>
-                    </>
-                ) : (
-                    <>
-                        <button onClick={() => navigate('/login')}><LogIn /> Login</button>
-                        <button onClick={() => navigate('/register')}><UserPlus /> Register</button>
-                    </>
-                )}
-            </div>
-            <img src={Logo} alt="Logo" draggable="false"/>
-        </div>
-    );
-}
-
-export default Head;
-<<<<<<< HEAD
-=======
->>>>>>> 6fb4cbabb18bdf363ddb9fdc66e5684e693227d1
->>>>>>> 0c76dc1 (Initial commit)
