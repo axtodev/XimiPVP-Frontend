751185607dbe5c16ee54dd22bcc16bd23c469e1e
axtodev <tony447113@gmail.com>
2026-02-06 19:18:29 +0100

    fix

---
+++ src/pages/stats.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import StatsComponent from '../components/StatsComponent';
+
+function Stats() {
+    return (
+        <StatsComponent />
+    );
+}
+
+export default Stats;
